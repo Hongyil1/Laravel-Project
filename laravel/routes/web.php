79138a88e02f77d2d239c791b8f9336d69947f53
Hongyil1 <hylxmu@163.com>
2019-03-27 00:31:19 +1100

    routh/web.php change

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/hello', function() {
+    return 'Hello, welcome to my website';
+});
