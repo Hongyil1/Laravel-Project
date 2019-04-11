ba0df8b15d6a23939a66915908759ae7051482f1
Hongyil1 <hylxmu@163.com>
2019-04-11 17:00:07 +1000

    Add Login and Register

--- myblog/resources/views/layouts/app.blade.php
+++ myblog/resources/views/layouts/app.blade.php
@@ -1,25 +1,36 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Fonts -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <!-- Styles -->
-        <title>{{ config('app.name', 'MyBlog') }}</title>
-    </head>
-    <body>
-      <div class="">
-          @include('inc.navbar')
-      </div>
-      <div class="container">
-          @include('inc.message')
-          @yield('content')
-      </div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-      <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
-      <script>
-          CKEDITOR.replace( 'article-ckeditor' );
-      </script>
-    </body>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'MyBlog') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <div>
+      @include('inc.navbar')
+    </div>
+    <div class="container">
+      @include('inc.message')
+      @yield('content')
+    </div>
+
+    <script src="/vendor/unisharp/laravel-ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
+</body>
 </html>
