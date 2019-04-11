62a2426b1a4437ddc24e15889295580accc10b97
Hongyil1 <hylxmu@163.com>
2019-04-11 10:32:08 +1000

    install laravel collective

--- myblog/resources/views/posts/create.blade.php
+++ myblog/resources/views/posts/create.blade.php
@@ -2,4 +2,10 @@
 
 @section('content')
   <h3>Create</h3>
+  {!! Form::open(['action' => 'PostController@store', 'method' => 'Post']) !!}
+    <div class="fore-group">
+      {{form::label('title', 'title')}}
+      {{form::text('title', '', ['class' => 'form-control', 'placeholder' => 'title'])}}
+    </div>
+  {!! Form::close() !!}
 @endsection
